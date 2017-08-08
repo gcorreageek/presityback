874103aebe2721877d80ea13ed4ebb85e82ff4f8
Gusta <gcorreageek@gmail.com>
2017-08-08 10:31:50 -0500

    nada no funciono! :(

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var Express = require( 'express' );
 var BodyParser = require( 'body-parser' );
 var login = require('./server/app/login');
+var token = require('./server/app/token');
 login = login();
 var app = Express( );
 
@@ -10,10 +11,20 @@ app.use( login.initialize( ) );
 app.post(
     '/login',
     login.authenticate( 'localMio', { session: false } ),
+    token()
+);
+app.get(
+    '/userinfo',
+    login.authenticate( 'bearer', { session: false } ),
     function ( request, response ) {
-        response.send( 'User Id ' + request.user.id );
+        var user = request.user;
+        response.send( {
+            id: user.id,
+            username: user.username
+        });
     }
 );
+
 app.listen( 8080, function( ) {
     console.log( 'Listening on port 8080' );
 });
